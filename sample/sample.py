ad633ad5a6606196bd56abdab64fd815377d923a
Josia Scheytt <jscheytt@googlemail.com>
2017-03-14 23:17:08 +0100

    Add logging to sample

--- sample/sample.py
+++ sample/sample.py
@@ -3,22 +3,28 @@ from sklearn import svm, metrics
 import feature_extraction.ft_descriptor as fd
 import label_import.label_importer as li
 import preprocessing.preprocessor as pre
+import logging
 
 
 def get_data_and_targets(xmlpath, labelspath):
     labels = li.get_labels_from_file(labelspath)
     video = fd.Video(xmlpath=xmlpath, labels=labels)
+    logging.info("Expand labels list ...")
     label_list = video.get_label_list()
+    logging.info("Labels list expanded.")
 
     ft_vec_list = video.get_featurevector_list()
+    logging.info("Import feature list ...")
     norm_ft_vec_list = pre.normalize_ft_vec_list(ft_vec_list, max_val=video.numpx)
+    logging.info("Feature list imported.")
     return norm_ft_vec_list, label_list
 
 
 def get_svclassifier(data, targets):
     clf = svm.SVC()
+    logging.info("Fit SVM to data ...")
     clf.fit(data, targets)
-
+    logging.info("SVM fitted.")
     return clf
 
 
