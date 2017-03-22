e5c8079d754af12ea9cd7b0a35e72f34001f0e93
Josia Scheytt <jscheytt@googlemail.com>
2017-03-22 21:15:24 +0100

    Add documentation to sample

--- sample/sample.py
+++ sample/sample.py
@@ -7,6 +7,12 @@ import logging
 
 
 def get_data_and_targets(xmlpath, labellistpath):
+    """
+    Get sklearn-conform lists of features (data) and labels (targets).
+    :param xmlpath: Path to the feature XML file
+    :param labellistpath: Path to the label list CSV file
+    :return:
+    """
     label_list = li.read_label_list(labellistpath)
     video = fd.Video(xmlpath=xmlpath, label_list=label_list)
     ft_vec_list = video.get_featurevector_list()
@@ -17,6 +23,12 @@ def get_data_and_targets(xmlpath, labellistpath):
 
 
 def get_svclassifier(data, targets):
+    """
+    Fit a SVM classifier to the given training data.
+    :param data: List of feature vectors
+    :param targets: List of labels
+    :return: The learned classifier
+    """
     clf = svm.SVC()
     logging.info("Fit SVM to data ...")
     clf.fit(data, targets)
@@ -25,6 +37,13 @@ def get_svclassifier(data, targets):
 
 
 def get_evaluation(classifier, data, targets):
+    """
+    Get an evaluation report on the prediction correctness of the classifier.
+    :param classifier:
+    :param data:
+    :param targets:
+    :return: String containing the evaluation report
+    """
     expected = targets
     predicted = classifier.predict(data)
     return metrics.classification_report(expected, predicted)
