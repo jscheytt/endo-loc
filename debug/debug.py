4740bb222834eb176f11452cd27cfe4f21a91cd1
Josia Scheytt <jscheytt@googlemail.com>
2017-03-25 20:15:08 +0100

    Turn debugging class functional, add logging context

--- debug/debug.py
+++ debug/debug.py
@@ -1,68 +1,78 @@
 import cv2
 from matplotlib import pyplot as plt
-
-import feature_extraction.ft_extractor as fx
+import logging
 
 IMG_TITLE = "image"
 
 
-class Debug:
-    @classmethod
-    def display_image(cls, image):
-        """Display image in a new window. Window closes after pressing any key."""
-        cv2.imshow(IMG_TITLE, image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+def display_image(image):
+    """Display image in a new window. Window closes after pressing any key."""
+    cv2.imshow(IMG_TITLE, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-    @classmethod
-    def get_histograms_rgb(cls, image):
-        """
-        Retrieve RGB histograms of an image.
-        :param image: OpenCV image object
-        :return: List of histograms in order red, green, blue.
-        """
-        hist_blue = cv2.calcHist([image], [0], None, [256], [0, 256])
-        hist_green = cv2.calcHist([image], [1], None, [256], [0, 256])
-        hist_red = cv2.calcHist([image], [2], None, [256], [0, 256])
-        return [hist_red, hist_green, hist_blue]
 
-    @classmethod
-    def plot_histograms(cls, hists, update_interval=0.0):
-        """
-        Plot histogram in new window in Red, Green and Blue.
-        :param hists: List of 3 histogram channels
-        :param update_interval: Time in seconds at which the window should be updated
-        :return:
-        """
-        color = ('r', 'g', 'b')
-        for idx, col in enumerate(color):
-            plt.plot(hists[idx], color=col)
-            plt.xlim([0, 256])
-        plt.draw()
-        plt.pause(update_interval)
+def get_histograms_rgb(image):
+    """
+    Retrieve RGB histograms of an image.
+    :param image: OpenCV image object
+    :return: List of histograms in order red, green, blue.
+    """
+    hist_blue = cv2.calcHist([image], [0], None, [256], [0, 256])
+    hist_green = cv2.calcHist([image], [1], None, [256], [0, 256])
+    hist_red = cv2.calcHist([image], [2], None, [256], [0, 256])
+    return [hist_red, hist_green, hist_blue]
 
-    @classmethod
-    def plot_histograms_live(cls, filename):
-        """
-        Plot histogram of a video frame by frame. Is not expected to work in real time.
-        :param filename: Path to video file
-        :return:
-        """
-        cap = cv2.VideoCapture(filename)
 
-        while cap.isOpened():
-            _, frame = cap.read()
+def plot_histograms(hists, update_interval=0.0):
+    """
+    Plot histogram in new window in Red, Green and Blue.
+    :param hists: List of 3 histogram channels
+    :param update_interval: Time in seconds at which the window should be updated
+    :return:
+    """
+    color = ('r', 'g', 'b')
+    for idx, col in enumerate(color):
+        plt.plot(hists[idx], color=col)
+        plt.xlim([0, 256])
+    plt.draw()
+    plt.pause(update_interval)
 
-            hists_frame = fx.FeatureExtractor.get_histograms_hsv(frame)
 
-            # Clear current plot
-            plt.clf()
-            plt.cla()
-            # Update at 25 fps speed if live
-            Debug.plot_histograms(hists_frame, 0.04)
+def plot_histograms_live(filename):
+    """
+    Plot histogram of a video frame by frame. Is not expected to work in real time.
+    :param filename: Path to video file
+    :return:
+    """
+    import feature_extraction.ft_extractor as fx
+    cap = cv2.VideoCapture(filename)
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+    while cap.isOpened():
+        _, frame = cap.read()
 
-        cap.release()
-        cv2.destroyAllWindows()
+        hists_frame = fx.get_histograms_hsv(frame)
+
+        # Clear current plot
+        plt.clf()
+        plt.cla()
+        # Update at 25 fps speed if live
+        plot_histograms(hists_frame, 0.04)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+class LogSurr:
+    def __init__(self, message):
+        self.message = message
+
+    def __enter__(self):
+        logging.info("{} ...".format(self.message))
+        return self
+
+    def __exit__(self, ctx_type, ctx_value, ctx_traceback):
+        logging.info("--- {} finished.".format(self.message))
--- tests/test_debug.py
+++ tests/test_debug.py
@@ -2,7 +2,7 @@ from .context import sample
 
 import pytest
 
-from debug.debug import Debug as Dbg
+import debug.debug as dbg
 import feature_extraction.ft_extractor as fx
 import tests.conftest as cft
 
@@ -13,28 +13,33 @@ def test_load_image(test_image):
 
 @pytest.mark.skip(reason="GUI")
 def test_display_image(test_image):
-    Dbg.display_image(test_image)
+    dbg.display_image(test_image)
 
 
 def test_get_histograms_rgb(test_image):
-    hists = Dbg.get_histograms_rgb(test_image)
+    hists = dbg.get_histograms_rgb(test_image)
     for hist in hists:
         assert len(hist)
 
 
 @pytest.mark.skip(reason="GUI")
 def test_plot_histogram_rgb(test_image):
-    hists = Dbg.get_histograms_rgb(test_image)
-    Dbg.plot_histograms(hists)
+    hists = dbg.get_histograms_rgb(test_image)
+    dbg.plot_histograms(hists)
 
 
 @pytest.mark.skip(reason="GUI")
 def test_plot_histogram_hsv(test_image):
     hists = fx.get_histograms_hsv(test_image)
-    Dbg.plot_histograms(hists)
+    dbg.plot_histograms(hists)
 
 
 @pytest.mark.skip(reason="GUI")
 def test_display_video_histogram_hsv():
-    Dbg.plot_histograms_live(cft.example_vid)
+    dbg.plot_histograms_live(cft.example_vid)
     # TODO Fail if not displayed
+
+
+def test_log_surr():
+    with dbg.LogSurr("Do this action"):
+        pass
