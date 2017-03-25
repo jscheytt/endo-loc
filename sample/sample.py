923efc1d105ba55253c92a1dc87ded3fd9b703ee
Josia Scheytt <jscheytt@googlemail.com>
2017-03-25 20:31:10 +0100

    Refactor old logging use

--- sample/sample.py
+++ sample/sample.py
@@ -3,7 +3,7 @@ from sklearn import svm, metrics
 import feature_extraction.ft_descriptor as fd
 import label_import.label_importer as li
 import preprocessing.preprocessor as pre
-import logging
+from debug.debug import LogCont
 
 
 def get_data_and_targets(xmlpath, labellistpath):
@@ -13,17 +13,13 @@ def get_data_and_targets(xmlpath, labellistpath):
     :param labellistpath: Path to the label list CSV file
     :return:
     """
-    logging.info("Import label list ...")
-    label_list = li.read_label_list(labellistpath)
-    logging.info("Label list imported.")
-    video = fd.Video(xmlpath=xmlpath, label_list=label_list)
-
-    logging.info("Import feature list ...")
-    ft_vec_list = video.get_featurevector_list()
-    logging.info("Feature list imported.")
-    logging.info("Preprocess feature list ...")
-    norm_ft_vec_list = pre.normalize_ft_vec_list(ft_vec_list, max_val=video.numpx)
-    logging.info("Feature list preprocessed.")
+    with LogCont("Import label list"):
+        label_list = li.read_label_list(labellistpath)
+    with LogCont("Import feature list"):
+        video = fd.Video(xmlpath=xmlpath, label_list=label_list)
+        ft_vec_list = video.get_featurevector_list()
+    with LogCont("Preprocess feature list"):
+        norm_ft_vec_list = pre.normalize_ft_vec_list(ft_vec_list, max_val=video.numpx)
     return norm_ft_vec_list, label_list
 
 
@@ -35,9 +31,8 @@ def get_svclassifier(data, targets):
     :return: The learned classifier
     """
     clf = svm.SVC(class_weight="balanced")
-    logging.info("Fit SVM to data ...")
-    clf.fit(data, targets)
-    logging.info("SVM fitted.")
+    with LogCont("Fit SVM to data"):
+        clf.fit(data, targets)
     return clf
 
 
@@ -50,5 +45,6 @@ def get_evaluation(classifier, data, targets):
     :return: String containing the evaluation report
     """
     expected = targets
-    predicted = classifier.predict(data)
+    with LogCont("Predict on test data"):
+        predicted = classifier.predict(data)
     return metrics.classification_report(expected, predicted)
