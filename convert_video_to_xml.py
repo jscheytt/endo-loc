ab9a725cb4f070ccc9161de664562f5e306cd2ee
Josia Scheytt <jscheytt@googlemail.com>
2017-08-03 18:28:23 +0200

    Make output filenames less ambiguous

--- convert_video_to_xml.py
+++ convert_video_to_xml.py
@@ -3,24 +3,31 @@ import argparse
 import feature_extraction.ft_extractor as fx
 import helper.helper as hlp
 
-DEF_TARGET_FILENAME_PATTERN = "{}_ft.xml"
+DEF_TARGET_FILENAME_PATTERN = "{}_{}.xml"
 
 
-def get_default_target_filename(video_filename):
+def get_default_target_filename(video_filename, h_c, s_c, v_c):
     prefix = hlp.strip_file_extension(video_filename)
-    return DEF_TARGET_FILENAME_PATTERN.format(prefix)
+    suffix = ""
+    if h_c:
+        suffix += "h"
+    if s_c:
+        suffix += "s"
+    if v_c:
+        suffix += "v"
+    return DEF_TARGET_FILENAME_PATTERN.format(prefix, suffix)
 
 
 def main(videofile, no_h_channel=False, no_s_channel=False, no_v_channel=False):
     hlp.setup_logging()
 
-    h = not no_h_channel
-    s = not no_s_channel
-    v = not no_v_channel
+    h_c = not no_h_channel
+    s_c = not no_s_channel
+    v_c = not no_v_channel
 
-    videoxml = fx.get_xml_from_videofile(videofile, h, s, v)
+    videoxml = fx.get_xml_from_videofile(videofile, h_c, s_c, v_c)
 
-    target_filename = get_default_target_filename(videofile)
+    target_filename = get_default_target_filename(videofile, h_c, s_c, v_c)
     fx.write_video_to_xml(videoxml, target_filename=target_filename)
 
 
